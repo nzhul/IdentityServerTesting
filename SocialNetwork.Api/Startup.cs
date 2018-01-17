2ac8c29fb54f3d6dbf5967d03e654c4ab15c1d9a
Dobromir Ivanov <dobromirivanov1@gmail.com>
2018-01-17 21:58:40 +0200

    update

--- SocialNetwork.Api/Startup.cs
+++ SocialNetwork.Api/Startup.cs
@@ -35,8 +35,11 @@ namespace SocialNetwork.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
