600ff87bebd416b881c9d4d7f27c40041e2cfbb2
Dobromir Ivanov <dobromirivanov1@gmail.com>
2018-01-22 13:55:22 +0200

    adding identity server with asp.net Identity

--- OfficialQuickStart/ServerWithAspNetIdentity/Startup.cs
+++ OfficialQuickStart/ServerWithAspNetIdentity/Startup.cs
@@ -37,6 +37,14 @@ namespace ServerWithAspNetIdentity
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddAspNetIdentity<ApplicationUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,7 +63,8 @@ namespace ServerWithAspNetIdentity
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
+            // app.UseAuthentication();
+            app.UseIdentityServer();
 
             app.UseMvc(routes =>
             {
